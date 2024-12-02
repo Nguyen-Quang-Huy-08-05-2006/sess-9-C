b2101fd8ba826ce27326120a39f5c37f92a14019
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-12-02 09:05:25 +0700

    Update btvn4Sess9.cpp

--- btvn4Sess9.cpp
+++ btvn4Sess9.cpp
@@ -1,91 +1,81 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main(){
-	int a,o;
-	while(1000000){
-	printf("MENU \n \n1.nhap vao mang. \n2.hien thi mang. \n3.them phan tu. \n4.sua phan tu. \n5.xoa phan tu. \n6.thoat. \ncau tra loi cua ban: ");
-	scanf("%d",a);
-	switch(a){
-		case 1:
-			printf("ban muon co bao nhieu phan tu: ");
-				scanf("%d",&o);
-				for(int i=0;i<o;i++){
-					printf("nhap gia tri phan tu thu %d: ",i+1);
-					int arr[i];
-					scanf("%d",&arr[i]);
-				}
-			break;
-		case 2:
-			printf("ban muon co bao nhieu phan tu: ");
-				scanf("%d",&o);
-				for(int i=0;i<o;i++){
-					printf("nhap gia tri phan tu thu %d: ",i+1);
-					int arr[i];
-					scanf("%d",&arr[i]);
-				}
-				for(int i=0;i<o+1;i++){
-				printf("%d ",arr[i]);
-			}
-			break;
-		case 3:
-			printf("ban muon co bao nhieu phan tu: ");
-				scanf("%d",&o);
-				for(int i=0;i<o;i++){
-					printf("nhap gia tri phan tu thu %d: ",i+1);
-					int arr[i];
-					scanf("%d",&arr[i]);
-				}
-				int m,n,o;
-			printf("nhap gia tri ban muon chen: ");
-				scanf("%d",&m);
-			printf("ban muon chen vao dau: ");
+	int o=0,arr[100],n,i=0,m,num,check=0;
+	while(o!=6){
+		i=0;
+		printf("\n   MENU\n\n   1. Nhap vao mang\n   2. Hien thi mang\n   3. Them phan tu\n   4. Sua phan tu\n   5. Xoa phan tu\n   6. Thoat\n\nLua chon cua ban: ");
+		scanf("%d",&o);
+		switch(o){
+			case 1:
+			    printf("vui long nhap vao so phan tu trong mang: ");
 				scanf("%d",&n);
-			for(int i=o;i>=n;i--){
-				arr[i+1]=arr[i];
-			}
-			arr[n]=m;
-			for(int i=0;i<o+1;i++){
-			printf("%d ",arr[i]);
-			}
-			break;
-		case 4:
-			printf("ban muon co bao nhieu phan tu: ");
-				scanf("%d",&o);
-				for(int i=0;i<o;i++){
-					printf("nhap gia tri phan tu thu %d: ",i+1);
-					int arr[i];
+				while(i<n){
+					printf("vui long nhap gia tri co vi tri %d: ",i);
 					scanf("%d",&arr[i]);
+					i++;
+					check=1;
 				}
-				int length=sizeof(arr)/sizeof(arr[0]);
-			int m,n;
-			printf("\nnhap vao vi tri can sua: ");
-			scanf("%d",&m);
-			printf("nhap vao gia tri cua vi tri do: ");
-			scanf("%d",&n);
-			arr[m]=n;
-			for(int i=0;i<length;i++){
-				printf("%d, ",arr[i]);
-			}
-			break;
-		case 5:
-			printf("ban muon co bao nhieu phan tu: ");
-			scanf("%d",&o);
-			for(int i=0;i<o;i++){
-				printf("nhap gia tri phan tu thu %d: ",i+1);
-				scanf("%d",&arr[i]);
-			}
-			printf("nhap vi tri ban muon xoa: ");
-			scanf("%d",&m);
-			for(int i=m;i<o;i++){
-				arr[i]=arr[i]+1;
-			}
-			for(int i=0;i<o-1;i++){
-				printf("%d ",arr[i]);
-			}
-			break;
-		default:
-			break;
-	}
+			    break;
+			case 2:
+				if(check){
+					while(i<n){
+						printf("%d ",arr[i]);
+						i++;
+					}
+				}
+			    break;
+			case 3:
+			    if(check){
+			    	printf("vui long nhap vi tri muon chen: ");
+					scanf("%d",&m);
+					if(m<n&&m>=0){
+						while(n-i>m){
+							arr[n-i]=arr[n-i-1];
+							i++;
+						}
+					}else{
+						printf("vi tri khong ton tai");
+						break;
+					}
+					printf("vui long nhap gia tri muon chen: ");
+					scanf("%d",&num);
+					arr[m]=num;
+					n++;
+				}
+			    break;
+			case 4:
+			    if(check){
+					printf("vui long nhap vi tri muon thay doi: ");
+					scanf("%d",&m);
+					if(m<n&&m>=0){
+						printf("vui long nhap gia tri muon thay doi: ");
+						scanf("%d",&num);
+						arr[m]=num;
+					}else{
+						printf("vi tri khong ton tai");
+						break;
+					}
+				}
+				break;
+			case 5:
+				if(check){
+					printf("vui long nhap vi tri muon xoa: ");
+					scanf("%d",&m);
+					i=m;
+					if(m<n&&m>=0){
+						while(i<n-1){
+							arr[i]=arr[i+1];
+							i++;
+						}
+					}else{
+						printf("vi tri khong ton tai");
+						break;
+					}
+					n--;
+				}
+				break;
+		}
 	}
 	return 0;
 }
-
